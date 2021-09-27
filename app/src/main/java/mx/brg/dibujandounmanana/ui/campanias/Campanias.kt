ce5cbba628b3d194f512aaede12f6241a9099871
Daniel <danielttgb44@live.com.mx>
2021-09-27 14:27:25 -0500

    Ventana de "Ver Campañas" añadida

---
+++ app/src/main/java/mx/brg/dibujandounmanana/ui/campanias/Campanias.kt
@@ -0,0 +1,32 @@
+package mx.brg.dibujandounmanana.ui.campanias
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mx.brg.dibujandounmanana.R
+
+class Campanias : Fragment() {
+
+    companion object {
+        fun newInstance() = Campanias()
+    }
+
+    private lateinit var viewModel: CampaniasViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_campanias, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CampaniasViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/mx/brg/dibujandounmanana/ui/campanias/CampaniasViewModel.kt
@@ -0,0 +1,7 @@
+package mx.brg.dibujandounmanana.ui.campanias
+
+import androidx.lifecycle.ViewModel
+
+class CampaniasViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
