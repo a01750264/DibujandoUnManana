0073b4d76443b5f277e445b37dcf51327967d800
Daniel <danielttgb44@live.com.mx>
2021-09-26 23:38:25 -0500

    Nueva ventana "¿Qué hacemos? y nuevos colores añadidos
    La nueva ventana está vacía

---
+++ app/src/main/java/mx/brg/dibujandounmanana/ui/quehacemos/QueHacemos.kt
@@ -0,0 +1,32 @@
+package mx.brg.dibujandounmanana.ui.quehacemos
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import mx.brg.dibujandounmanana.R
+
+class QueHacemos : Fragment() {
+
+    companion object {
+        fun newInstance() = QueHacemos()
+    }
+
+    private lateinit var viewModel: QueHacemosViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_que_hacemos, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(QueHacemosViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/mx/brg/dibujandounmanana/ui/quehacemos/QueHacemosViewModel.kt
@@ -0,0 +1,7 @@
+package mx.brg.dibujandounmanana.ui.quehacemos
+
+import androidx.lifecycle.ViewModel
+
+class QueHacemosViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
