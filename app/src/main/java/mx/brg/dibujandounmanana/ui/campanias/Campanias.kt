984d88a0350f6abeb823e1636e65dfdcd3fa5671
a01750264 <a01750246@itesm.mx>
2021-10-07 21:37:11 -0500

    Pantalla de campañas avance

--- app/src/main/java/mx/brg/dibujandounmanana/ui/campanias/Campanias.kt
+++ app/src/main/java/mx/brg/dibujandounmanana/ui/campanias/Campanias.kt
@@ -1,5 +1,7 @@
 package mx.brg.dibujandounmanana.ui.campanias
 
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,8 +9,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import mx.brg.dibujandounmanana.R
+import mx.brg.dibujandounmanana.databinding.FragmentCampaniasBinding
 
 class Campanias : Fragment() {
+
+    private lateinit var binding: FragmentCampaniasBinding
 
     companion object {
         fun newInstance() = Campanias()
@@ -20,13 +25,24 @@ class Campanias : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_campanias, container, false)
+        binding = FragmentCampaniasBinding.inflate(layoutInflater)
+        val vista = binding.root
+        return vista
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CampaniasViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view , savedInstanceState)
+        binding.btnVerMas2.setOnClickListener{
+            val urlIntent = Intent(Intent.ACTION_VIEW)
+            urlIntent.data = Uri.parse("https://www.dibujando.org.mx/personas/")
+            startActivity(urlIntent)
+        }
+
+        binding.btnVerMasJuntos.setOnClickListener{
+            val urlIntent = Intent(Intent.ACTION_VIEW)
+            urlIntent.data = Uri.parse("https://www.dibujando.org.mx/")
+            startActivity(urlIntent)
+        }
     }
 
 }
