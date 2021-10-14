aaca86873fc31e9dcfe3989b0431ed560a4e2a5f
Daniel <danielttgb44@live.com.mx>
2021-10-13 21:27:11 -0500

    Intro añadida
    Aún no desaparece cuando se cierra la app y probablemente cambien los colores

--- app/src/main/java/mx/brg/dibujandounmanana/intro/IntroActivity.kt
+++ app/src/main/java/mx/brg/dibujandounmanana/intro/IntroActivity.kt
@@ -2,14 +2,29 @@ package mx.brg.dibujandounmanana.intro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import mx.brg.dibujandounmanana.R
+import mx.brg.dibujandounmanana.intro.ViewPagerAdapter
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+        supportActionBar?.hide()
+
         val viewPager: ViewPager2 = findViewById(R.id.vpIntro)
+
+        val fragments: ArrayList<Fragment> = arrayListOf(
+            IntroPage1Fragment(),
+            IntroPage2(),
+            IntroPage3()
+        )
+
+        val adapter = ViewPagerAdapter(fragments, this)
+        viewPager.adapter = adapter
+
+        
     }
 }
--- app/src/main/java/mx/brg/dibujandounmanana/intro/IntroPage3.kt
+++ app/src/main/java/mx/brg/dibujandounmanana/intro/IntroPage3.kt
@@ -1,11 +1,17 @@
 package mx.brg.dibujandounmanana.intro
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.setContentView
 import mx.brg.dibujandounmanana.R
+import mx.brg.dibujandounmanana.RegistarActivity
+import mx.brg.dibujandounmanana.databinding.ActivityLoginBinding
+import mx.brg.dibujandounmanana.databinding.FragmentIntroPage3Binding
+import mx.brg.dibujandounmanana.ui.LoginActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,20 +28,31 @@ class IntroPage3 : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var binding: FragmentIntroPage3Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_intro_page3, container, false)
+        // As the binding class will change, binding inflate method will also change from fragment to fragment
+        binding = FragmentIntroPage3Binding.inflate(inflater, container, false)
+
+        binding.btnEmpecemos.setOnClickListener {
+            val loginActivity = Intent(activity, LoginActivity::class.java)
+            activity?.startActivity(loginActivity)
+            activity?.finish()
+        }
+        return binding.root
     }
 
     companion object {
