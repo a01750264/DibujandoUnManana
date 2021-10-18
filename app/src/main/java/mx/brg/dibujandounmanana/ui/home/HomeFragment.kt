3dfb5b75c75d613f73008a280280e060c1079fbb
Daniel <danielttgb44@live.com.mx>
2021-10-18 14:42:56 -0500

    Cambio en el inicio

--- app/src/main/java/mx/brg/dibujandounmanana/ui/home/HomeFragment.kt
+++ app/src/main/java/mx/brg/dibujandounmanana/ui/home/HomeFragment.kt
@@ -8,37 +8,31 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import mx.brg.dibujandounmanana.databinding.FragmentContactanosBinding
 import mx.brg.dibujandounmanana.databinding.FragmentHomeBinding
+import android.content.Intent
+import android.net.Uri
+
 
 class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
-    private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        val vista = binding.root
+        return vista
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnVideo.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=p2lkxWtMd-o")))
+        }
     }
 }
