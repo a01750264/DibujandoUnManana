db4df3493085c30055e05febd4b6db0557b78dd2
Daniel <danielttgb44@live.com.mx>
2021-10-17 23:04:11 -0500

    Pantallas de editar campañas e iniciativas añadidas y elementos de front a Main Admin

--- app/src/main/java/mx/brg/dibujandounmanana/admin/MainAdminActivity.kt
+++ app/src/main/java/mx/brg/dibujandounmanana/admin/MainAdminActivity.kt
@@ -1,12 +1,31 @@
 package mx.brg.dibujandounmanana.admin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mx.brg.dibujandounmanana.R
+import mx.brg.dibujandounmanana.databinding.ActivityEditarCampaniasBinding
+import mx.brg.dibujandounmanana.databinding.ActivityEditarIniciativasBinding
+import mx.brg.dibujandounmanana.databinding.ActivityMainAdminBinding
 
 class MainAdminActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainAdminBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_admin)
+
+        binding = ActivityMainAdminBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnEditarCampanias.setOnClickListener {
+            val editCampAdminActivity = Intent(this, EditarCampaniasActivity::class.java)
+            startActivity(editCampAdminActivity)
+        }
+
+        binding.btnEditarIniciativas.setOnClickListener {
+            val editIniAdminActivity = Intent(this, EditarIniciativasActivity::class.java)
+            startActivity(editIniAdminActivity)
+        }
     }
 }
